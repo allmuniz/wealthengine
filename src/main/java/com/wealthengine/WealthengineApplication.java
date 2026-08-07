0d61ce157d2ed49b32dfdf432d8cfd200c505c1c
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2026-08-06 21:09:55 -0300

    First commit, adds Dockerfile and docker-compose

---
+++ src/main/java/com/wealthengine/WealthengineApplication.java
@@ -0,0 +1,13 @@
+package com.wealthengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WealthengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WealthengineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wealthengine/WealthengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wealthengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WealthengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
